78a0e05e614a7cb658ce7152cd2b77939ad42a40
Jakub Wilk <jw@jakubwilk.pl>
2020-11-16 17:04:47 +0100

    Edit connection to db; Found bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,33 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  	imports: [
+		ConfigModule.forRoot({
+			envFilePath: ['.env.development'],
+		}),
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: process.env['DATABASE_HOST'],
+			port: parseInt(process.env['DATABASE_PORT']),
+			username: process.env['DATABASE_USER'],
+			password: process.env['DATABASE_PASSWORD'],
+			database: process.env['DATABASE_NAME'],
+			synchronize: false,
+			entities: [],
+			migrations: ['src/migrations/**/*.js'],
+			cli: {
+				migrationsDir: 'src/migrations'
+			}
+		})
+  	],
+  	controllers: [AppController],
+  	providers: [AppService],
 })
 export class AppModule {
-  constructor(private connection: Connection) {}
+  	constructor(private connection: Connection) {}
 }
