0c2eb0615f563b7dbcdf8ccb10ff577a19c4adaf
Jakub Wilk <jw@jakubwilk.pl>
2021-01-06 14:48:32 +0100

    Update tsconfig

--- src/users-auth/users-auth.service.ts
+++ src/users-auth/users-auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUser, EditUser } from 'src/types/users-auth.types';
 import { Repository } from 'typeorm';
@@ -39,7 +39,7 @@ export class UsersAuthService {
 
     async editUser(userData: EditUser): Promise<boolean> {
         const { userId, email, password, role, isActive, isBanned } = userData;
-        const user = await this.usersAuthRepository.findOne({ id: userId });
+        const user = await this.usersAuthRepository.findOne({ id: userId }) as Users;
         user.email = email;
         user.password = password;
         user.role = role;
@@ -58,7 +58,7 @@ export class UsersAuthService {
     async deleteUser(userId: string): Promise<boolean> {
         // Don't remove data from database, but add a special flag to hide and off all "deleted" account data
         // Todo: delete user tokens
-        const user = await this.usersAuthRepository.findOne({ id: userId });
+        const user = await this.usersAuthRepository.findOne({ id: userId }) as Users;
         user.is_active = false;
         const deleteAction = await this.usersAuthRepository.save(user);
 
